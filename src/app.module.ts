4ffe60ee5db7adba131dd7b5936204db89855b46
Tomer Babila <tomerb@moveo.co.il>
2022-12-27 17:54:35 +0200

    Add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
+import Joi from '@hapi/joi';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(27017),
+      }),
+    }),
     CoffeesModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-course'),
+    MongooseModule.forRoot(
+      `mongodb://${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/nest-course`,
+    ),
     CoffeeRatingModule,
   ],
   controllers: [AppController],
